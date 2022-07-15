23ae6266108c31d273372007e4722af1533887c2
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-15 13:13:38 +0300

    Refactored 3 classes to be situated in a separate package & used @ComponentScan
    Took 7 minutes

--- src/main/java/guru/springframework/sfgdi/controllers/PetController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PetController.java
@@ -1,6 +1,6 @@
 package guru.springframework.sfgdi.controllers;
 
-import guru.springframework.sfgdi.services.PetService;
+import vlad.springframework.sfgdi.PetService;
 import org.springframework.stereotype.Controller;
 
 /**
